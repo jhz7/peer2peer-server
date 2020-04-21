6c2d7adb93772960634a1d2959ee23e796313871
jhz7 <jhzambrano7@gmail.com>
2020-04-21 07:47:39 -0500

    Se completa desarrollo funcional de video llamada
    - Se logra implmentar comunicacion peer to peer
    - Se resuelve bug en pambiente productivo debido a que se instanciaba el socket 2 veces en el cliente.
    
    Finalización del demo

--- public/js/socket.client.js
+++
@@ -1,9 +0,0 @@
-var socket = io();
-
-socket.on('connect', function() {
-  console.log('Conectado al servidor');
-});
-
-socket.on('disconnect', function(){
-  console.log('Conexión con el servidor perdida');
-});
